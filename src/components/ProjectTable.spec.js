4ccdfa901bbb22d81a53287f1c4613cbee392201
tobi <krisella74@gmail.com>
2021-05-16 13:45:44 +0100

    show view project button when project name is clicked

--- src/components/ProjectTable.spec.js
+++ src/components/ProjectTable.spec.js
@@ -1,6 +1,42 @@
 import { mount } from "@vue/test-utils";
 import ProjectTable from "./ProjectTable.vue";
 
+const testData = [
+  {
+    id: 1,
+    country: "Togo",
+    developer: "Kerluke, Howe and Hammes PLC",
+    project_name: "Wuckert, Blanda and Crooks",
+    sector: "Institutional/Public Sector",
+    address: "16 Mohr Street 36215 AdamVille",
+    region: "SvenVille",
+    lga: "KamronVille",
+    status: "On hold",
+    main_contractor: "Gleason Group and Sons",
+    architect: "Macejkovic-Donnelly Inc",
+    quantity_surveyor: "Weimann, Douglas and Mertz PLC",
+    structural_engineers: "Turcotte, Sawayn and Hansen Group",
+    mep_engineer: "Howell-Hermiston LLC",
+    project_manager: "Reichert, Oberbrunner and Hettinger and Sons",
+    lift_contractor: "Zemlak, Hane and Kutch and Sons",
+    bulk_earth_contractor: "Kuvalis Inc Group",
+    other: null,
+    author: 1,
+    content_status: "publish",
+    deleted_at: null,
+    created_at: "2019-12-10 17:28:33",
+    updated_at: "2019-12-10 17:28:33",
+    latitude: "-75.287715",
+    longitude: "63.447841",
+    est_range: "$3M - $46M",
+    state: "Wyoming",
+    floors: 3,
+    elec_engineer: "Towne PLC Ltd",
+    piling_contractor: "O'Connell Ltd and Sons",
+    legal_partners: "Kutch-Watsica PLC",
+  },
+];
+
 describe("Project Table component", () => {
   test("renders a table element", () => {
     const table = mount(ProjectTable, {
@@ -9,5 +45,19 @@ describe("Project Table component", () => {
       },
     });
     expect(table.find("table").exists()).toBe(true);
+  });
+
+  test("display `view project` button when a project name is clicked", async () => {
+    const table = mount(ProjectTable, {
+      propsData: {
+        data: testData,
+      },
+    });
+    const firstRow = table.find("tbody tr:first-child");
+    // before it's clicked
+    expect(firstRow.find(".view-project").exists()).toBe(false);
+    await firstRow.find("td.project-name-column").trigger("click");
+    // after clicked;
+    expect(firstRow.find(".view-project").exists()).toBe(false);
   });
 });
--- src/components/ProjectTable.vue
+++ src/components/ProjectTable.vue
@@ -8,25 +8,31 @@
           <th>Main contractor</th>
           <th>Area</th>
           <th>State</th>
-          <th>Status</th>
+          <th class="status-column">Status</th>
           <th>Sector</th>
         </tr>
       </thead>
       <tbody>
         <tr
           :key="row.id"
+          :class="{ selected: selected === index }"
           v-for="(row, index) of data"
           :style="{
             animationDelay: getAnimationDelay(index),
           }"
         >
-          <td class="project-name-column">{{ row.project_name }}</td>
+          <td class="project-name-column" @click="selectProject(index)">
+            {{ row.project_name }}
+          </td>
           <td>{{ row.developer }}</td>
           <td>{{ row.main_contractor }}</td>
           <td>{{ row.lga }}</td>
           <td>{{ row.state }}</td>
           <td>{{ row.status }}</td>
           <td>{{ row.sector }}</td>
+          <div class="view-project" v-if="selected === index">
+            <button>View Project</button>
+          </div>
         </tr>
       </tbody>
     </table>
@@ -42,7 +48,16 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      selected: null,
+    };
+  },
   methods: {
+    selectProject(index) {
+      // if already selected, unselect
+      this.selected = this.selected === index ? null : index;
+    },
     getAnimationDelay(index) {
       const interval = 0.18; // 0.18 second
       return (index + 1) * interval + "s";
@@ -77,26 +92,77 @@ table {
     }
   }
   td {
-    padding: 15px 10px;
+    padding: 20px 10px;
     background: #fff;
   }
   tr {
+    position: relative;
     animation: moon-slide 0.5s ease;
     animation-fill-mode: both;
   }
   tr:nth-child(2n + 1) td {
     background: #f4f4f4;
   }
+
+  tr.selected td:not(.project-name-column) {
+    filter: blur(3px);
+  }
+
+  .project-name-column {
+    position: sticky;
+    left: 0;
+    font-weight: 600;
+    z-index: 4;
+    box-shadow: 1px 0 2px rgba(0, 0, 0, 0.09);
+    @media (max-width: 640px) {
+      position: static;
+      box-shadow: none;
+    }
+  }
+
+  td.project-name-column {
+    cursor: pointer;
+    user-select: none;
+  }
+
+  th.status-column {
+    /* mimick a button & an dropdown icon */
+    &::after {
+      content: "";
+      display: inline-block;
+      border: 5px solid transparent;
+      border-left-color: #fff;
+      transform: rotate(90deg) translateX(25%);
+      margin-left: 10px;
+      cursor: pointer;
+    }
+  }
 }
 
-.project-name-column {
-  position: sticky;
+.view-project {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  position: absolute;
+  z-index: 2;
+  top: 0;
+  bottom: 0;
   left: 0;
-  font-weight: 600;
-  box-shadow: 1px 0 2px rgba(0, 0, 0, 0.09);
-  @media (max-width: 640px) {
-    position: static;
-    box-shadow: none;
+  right: 0;
+  height: 100%;
+  background: transparent;
+
+  button {
+    border: none;
+    outline: none;
+    cursor: pointer;
+    border-radius: 5px;
+    background: #fff;
+    border: 1px solid var(--primary-color);
+    height: 40px;
+    padding: 0 20px;
+    font-size: 16px;
   }
 }
 
